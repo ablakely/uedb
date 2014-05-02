e081276016c62c6d2c02329c753f129d0ab48368
Aaron Blakely <aaron@ephasic.org>
2014-05-02 15:45:45 -0500

    Minor changes.

--- routes/view.js
+++ routes/view.js
@@ -1,6 +1,6 @@
 module.exports = function(app,uedb) {
 	app.get('/location/:lid', function(req, res) {
-		uedb.Location.findOne({locationid: req.params.lid}, function(location) {
+		uedb.Location.findOne({locationid: req.params.lid}, function(err, location) {
 			res.render("view", {l: location, conf: uedb.config})
 		})
 	}) 
